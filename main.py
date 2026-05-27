f0b15d9db04a617976f4d448924c0f5b4a76af61
PlatoJobs <platojobs@qq.com>
2026-05-27 13:22:08 +0800

    Rename README section headings to English

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ def add_md_top(repo, md, me):
     if not TOP_ISSUES_LABELS or not top_issues:
         return
     with open(md, "a+", encoding="utf-8") as md:
-        md.write("## 置顶文章\n")
+        md.write("## Featured Posts\n")
         for issue in top_issues:
             if is_me(issue, me):
                 add_issue_info(issue, md)
@@ -179,7 +179,7 @@ def add_md_recent(repo, md, me, limit=5):
     with open(md, "a+", encoding="utf-8") as md:
         # one the issue that only one issue and delete (pyGitHub raise an exception)
         try:
-            md.write("## 最近更新\n")
+            md.write("## Recent Updates\n")
             for issue in repo.get_issues():
                 if is_me(issue, me):
                     add_issue_info(issue, md)
